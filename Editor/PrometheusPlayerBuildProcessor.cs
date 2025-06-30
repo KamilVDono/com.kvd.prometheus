e5f4148609a9284e716b6a6f91ba94ef2bbb5c2c
KamilVDono <kamilvdono@gmail.com>
2025-06-30 17:38:41 +0200

    PrometheusPlayerBuildProcessor

---
+++ Editor/PrometheusPlayerBuildProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEditor.Build;
+
+namespace KVD.Prometheus.Editor
+{
+	public class PrometheusPlayerBuildProcessor : BuildPlayerProcessor
+	{
+		public override void PrepareForBuild(BuildPlayerContext buildPlayerContext)
+		{
+			var settings = PrometheusSettings.Instance;
+
+			if (settings.buildWithPlayer)
+			{
+				PrometheusBuilder.BuildPrometheus();
+			}
+
+			if (Directory.Exists(PrometheusPersistence.BaseDirectoryPath))
+			{
+				buildPlayerContext.AddAdditionalPathToStreamingAssets(PrometheusPersistence.BaseDirectoryPath, PrometheusPersistence.MainFolderName);
+			}
+		}
+	}
+}
